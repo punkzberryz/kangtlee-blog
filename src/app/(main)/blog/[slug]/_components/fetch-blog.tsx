390d96af9bb62ff5b9a6cb777caaa8b11a3a2c44
punkzberryz <16286841+punkzberryz@users.noreply.github.com>
2024-08-15 08:49:38 +0700

    add fetch blog

---
+++ src/app/(main)/blog/[slug]/_components/fetch-blog.tsx
@@ -0,0 +1,43 @@
+import { db } from "@/lib/db";
+import { cn, delay } from "@/lib/utils";
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
+import { getPost, getPostError } from "./get-post";
+
+interface FetchBlogProps {
+  slug: string;
+}
+export const FetchBlog = ({ slug }: FetchBlogProps) => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <FetchBlogAsync slug={slug} />
+    </Suspense>
+  );
+};
+
+const FetchBlogAsync = async ({ slug }: FetchBlogProps) => {
+  await delay(2000);
+  const { post, error } = await getPost(slug);
+  if (error) {
+    if (error.message === getPostError.postNotFound) {
+      return notFound();
+    }
+    throw new Error();
+  }
+  if (!post.content) {
+    return notFound();
+  }
+  console.log("found post!!");
+  // console.log({ post });
+
+  return (
+    <div
+      dangerouslySetInnerHTML={{ __html: post.content }}
+      className={cn(
+        "ProseMirror",
+        "relative min-h-[500px] w-full max-w-screen-lg border-muted bg-background sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:shadow-lg",
+        "prose prose-lg dark:prose-invert prose-headings:font-title font-default max-w-full focus:outline-none",
+      )}
+    ></div>
+  );
+};
--- src/app/(main)/blog/[slug]/page.tsx
+++ src/app/(main)/blog/[slug]/page.tsx
@@ -1,57 +1,60 @@
-import { notFound } from "next/navigation";
 import { db } from "@/lib/db";
-import { cn, delay } from "@/lib/utils";
-import { Suspense } from "react";
-
-const BlogAsync = async ({ slug }: { slug: string }) => {
-  await delay(2000);
-  const post = await db.post.findUnique({
-    where: {
-      slug: slug,
-    },
-  });
-  if (!post?.content) {
-    return notFound();
-  }
-  console.log("found post!!");
-  // console.log({ post });
-
-  return (
-    <div
-      dangerouslySetInnerHTML={{ __html: post.content }}
-      className={cn(
-        "ProseMirror",
-        "relative min-h-[500px] w-full max-w-screen-lg border-muted bg-background sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:shadow-lg",
-        "prose prose-lg dark:prose-invert prose-headings:font-title font-default max-w-full focus:outline-none",
-      )}
-    ></div>
-  );
-};
-
-const BlogPageBySlug = ({
-  params: { slug },
-}: {
+import { Metadata } from "next";
+import { FetchBlog } from "./_components/fetch-blog";
+import { getPost } from "./_components/get-post";
+interface BlogPageBySlugProps {
   params: {
     slug: string;
   };
-}) => {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <BlogAsync slug={slug} />
-    </Suspense>
-  );
+}
+
+const BlogPageBySlug = ({ params: { slug } }: BlogPageBySlugProps) => {
+  return <FetchBlog slug={slug} />;
 };
 
 export default BlogPageBySlug;
 
-// export async function generateStaticParams() {
-//   const posts = await getPosts();
-//   return posts.map((p) => ({ slug: p?.slug }));
-// }
 export async function generateStaticParams() {
-  console.log("fetching many posts");
-  await delay(2000);
   const posts = await db.post.findMany();
-  console.log("found many posts");
   return posts.map((p) => ({ slug: p?.slug }));
+}
+
+export async function generateMetadata({
+  params: { slug },
+}: BlogPageBySlugProps): Promise<Metadata> {
+  const { post, error } = await getPost(slug);
+  if (error) {
+    return {};
+  }
+  if (!post.content) {
+    return {};
+  }
+  return {
+    title: "Blog",
+    description: "Blog description",
+    openGraph: {
+      title: "Blog",
+      description: "Blog description",
+      url: "https://example.com/blog",
+      siteName: "Blog",
+      locale: "th_TH",
+      type: "website",
+      images: [
+        {
+          url: "https://example.com/og-image.jpg",
+          width: 1200,
+          height: 630,
+          alt: "Og Image Alt",
+        },
+      ],
+    },
+    twitter: {
+      title: "Blog",
+      card: "summary_large_image",
+      creator: "@creator",
+    },
+    icons: {
+      shortcut: "/favicon.ico",
+    },
+  };
 }
