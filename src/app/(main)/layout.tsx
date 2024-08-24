6196aa200af905867a3ed856a4e3e8ebe219160f
kangtlee <16286841+punkzberryz@users.noreply.github.com>
2024-08-24 21:59:08 +0700

    add google analyric

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,8 +1,15 @@
 import { MainLayout as MainLayoutWrapper } from "@/components/navbar";
 import React from "react";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { config } from "@/lib/config";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
-  return <MainLayoutWrapper>{children}</MainLayoutWrapper>;
+  return (
+    <>
+      <MainLayoutWrapper>{children}</MainLayoutWrapper>
+      <GoogleAnalytics gaId={config.googleAnalyticsId} />
+    </>
+  );
 };
 
 export default MainLayout;
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,12 +1,13 @@
 export const config = {
   projectName: "kangtlee-blog",
-  baseUrl: process.env.NEXT_PUBLIC_URL ?? "http://localhost:3000",
+  baseUrl: process.env.NEXT_PUBLIC_URL || "http://localhost:3000",
   signUpAdminSecret: process.env.SIGNUP_ADMIN_SECRET,
   cloudinary: {
     apiSecret: process.env.CLOUDINARY_API_SECRET,
     apiKey: process.env.CLOUDINARY_API_KEY,
     cloudName: process.env.NEXT_PUBLIC_CLOUDINARY_NAME,
   },
+  googleAnalyticsId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || "",
 };
 
 export const COOKIE_NAME = `${config.projectName}-session`;
