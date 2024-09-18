314520fc434438877f70755d798e1d6af50e3b44
kangtlee <16286841+punkzberryz@users.noreply.github.com>
2024-09-18 18:00:37 +0700

    correcting robot

--- src/lib/auth/oauth-setup.ts
+++ src/lib/auth/oauth-setup.ts
@@ -1,12 +1,13 @@
 import { Google, Line } from "arctic";
+import { config } from "../config";
 
 export const google = new Google(
   process.env.GOOGLE_CLIENT_ID || "",
   process.env.GOOGLE_CLIENT_SECRET || "",
-  process.env.GOOGLE_CALLBACK_URL || "",
+  `${config.baseUrl}/admin/signin/google/callback`,
 );
 export const line = new Line(
   process.env.LINE_CLIENT_ID || "",
   process.env.LINE_CLIENT_SECRET || "",
-  process.env.LINE_CALLBACK_URL || "",
+  `${config.baseUrl}/admin/signin/line/callback`,
 );
